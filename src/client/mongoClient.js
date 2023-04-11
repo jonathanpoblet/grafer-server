0052466f9d2b8df195d58ec7bddf79825d3aec39
Jonathan Poblet <jonathanpoblet228@gmail.com>
2023-04-11 15:18:01 -0300

    resolving mongo problems

--- src/client/mongoClient.js
+++ src/client/mongoClient.js
@@ -1,7 +1,7 @@
 import { MongoClient } from 'mongodb';
 import { portMongo, usernameMongo, passwordMongo, databaseMongo} from '../config/config.js';
 
-const uri = 'mongodb+srv://adminGraciela:adminFernandez@grafer.klqkvnf.mongodb.net/ecommerce.products'
+const uri = 'mongodb+srv://adminGraciela:adminFernandez@grafer.klqkvnf.mongodb.net'
 
 //`mongodb+srv://adminGraciela:adminFernandez@grafer.klqkvnf.mongodb.net`;
 
