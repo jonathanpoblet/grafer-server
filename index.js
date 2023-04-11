daf42f1ef45ea3be5d37e346f37471ea8d7b27ee
Jonathan Poblet <jonathanpoblet228@gmail.com>
2023-04-11 13:19:54 -0300

    finished

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import http from "http";
-import { app } from "./app.js";
-import { PORT } from "./config/config.js";
+import { app } from "./src/app.js";
+import { PORT } from "./src/config/config.js";
 
 const server = http.createServer(app);
 
