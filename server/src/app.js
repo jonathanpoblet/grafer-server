2a688fb8dd52d0a9f104f70bbb74ca72eb5c3987
Jonathan Poblet <jonathanpoblet228@gmail.com>
2023-04-11 12:52:19 -0300

    finished 1

--- server/src/app.js
+++ server/src/app.js
@@ -3,20 +3,13 @@ import cors from "cors";
 import { routerContact } from "./routers/routerContact.js";
 import { routerProducts } from "./routers/routerProducts.js";
 import { routerMPago } from "./routers/routerMPago.js";
-import { PORT } from "./config/config.js";
-
 
 export const app = express();
-
-app.listen(PORT)
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 app.use(cors({origin: {}}))
 
-app.get("/", (req,res) => {
-    res.json({success: success})
-})
 app.use("/api/products", routerProducts);
 app.use("/api/contact", routerContact);
 app.use("/mpago",routerMPago);
