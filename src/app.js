43338ec5eb6b490071b6d8c63d9246d2c5d82743
Jonathan Poblet <jonathanpoblet228@gmail.com>
2023-04-11 14:02:27 -0300

    testing products

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { routerContact } from "./routers/routerContact.js";
-// import { routerProducts } from "./routers/routerProducts.js";
+import { routerProducts } from "./routers/routerProducts.js";
 import { routerMPago } from "./routers/routerMPago.js";
 
 export const app = express();
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(cors({origin: {}}))
 
 app.use("/api/contact", routerContact);
-// app.use("/api/products", routerProducts);
+app.use("/api/products", routerProducts);
 app.use("/mpago",routerMPago);
 app.use('/', (req,res) => {
     res.json({ok: 200})
